e8631b9d87fa044f6edb0979ce1a9bb4306ff554
ramankarki <ramankarki40@gmail.com>
2020-08-29 19:59:50 +0545

    using array methods

--- basic/array.js
+++ basic/array.js
@@ -2,3 +2,8 @@ let array = ["hello world", 11];
 console.log(array);
 let arrayMul = [["blue", 4], ["white", 5]];
 console.log(arrayMul);
+
+var threeArr = [1, 4, 6];
+var oneDown = threeArr.pop();
+console.log(oneDown); // Returns 6
+console.log(threeArr); // Returns [1, 4]
