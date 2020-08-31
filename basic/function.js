e2725f9b2f8e5f5ae392013885aaecbd04a27c52
ramankarki <ramankarki40@gmail.com>
2020-08-31 13:00:36 +0545

    playing with array methods in function

--- basic/function.js
+++ basic/function.js
@@ -1,5 +1,36 @@
 function reuse(first, second){
+    "add numbers";
     console.log(first, second);
 }
 
 reuse("hello", "world");
+
+// ---------------------------------------------------------------------------------
+
+function hello(){
+    a = 10;
+    b = 10;
+    console.log(a+b);
+}
+
+
+hello();
+console.log(a);
+d = 50;
+console.log(d);
+
+// -----------------------------------------------------------------------------------
+
+function nextInLine(arr, item) {
+    arr.push(item) // add item at last
+    item = arr.shift(); // remove item from first
+    return item;
+}
+
+
+// Setup
+var testArr = [1,2,3,4,5];
+console.log(testArr)
+nextInLine(testArr, 10);
+console.log(testArr)
+
