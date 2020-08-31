ac163adeb70d0881479f9aed923131932760d63b
ramankarki <ramankarki40@gmail.com>
2020-08-31 13:30:35 +0545

    finished last challenge of basic js in freeCodeCamp

--- basic/recursion.js
+++ basic/recursion.js
@@ -8,5 +8,33 @@ function countdown(n){
     }
 }
 
-console.log(countdown(10));
+// console.log(countdown(10));
+
+// --------------------------------------------------------------------------------------------------------------------
+
+function countup(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const countArray = countup(n - 1);
+      countArray.push(n);
+      return countArray;
+    }
+}
+
+// console.log(countup(5)); // [ 1, 2, 3, 4, 5 ]
+
+// --------------------------------------------------------------------------------------------------------------------
+
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum > endNum) {
+      return [];
+    } else {
+      const countArray = rangeOfNumbers(startNum, endNum - 1);
+      countArray.push(endNum);
+      return countArray;
+    }
+}
+
+console.log(rangeOfNumbers(3, 5)); // [ 1, 2, 3, 4, 5 ]
 
