8baeb4acc6cbaeda2d9757ecb511cdd843d02c22
ramankarki <ramankarki40@gmail.com>
2020-09-01 19:41:47 +0545

    using spread operator and reduce method

--- basic/arrowFunction.js
+++ basic/arrowFunction.js
@@ -3,6 +3,23 @@ const arrow = (a) => a * 2;
 console.log(arrow(a));
 
 // --------------------------------------------------------------------------------------------
+
 const greeting = (name = "Anonymous") => "Hello " + name;
 console.log(greeting());
+
+// --------------------------------------------------------------------------------------------
+
+function functionName(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+}
+functionName("hello", "hi", "bye");
+
+// --------------------------------------------------------------------------------------------
+
+const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0); // this 0 is used as a second value to return 0 when no arguments are given in sum function.
+}
+console.log(sum());
 
