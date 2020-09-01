72851edf2d20a0d0f7767efb42713ad9872a06fd
ramankarki <ramankarki40@gmail.com>
2020-09-01 16:32:01 +0545

    working with arrow function

---
+++ basic/arrowFunction.js
@@ -0,0 +1,8 @@
+const a = 10
+const arrow = (a) => a * 2;
+console.log(arrow(a));
+
+// --------------------------------------------------------------------------------------------
+const greeting = (name = "Anonymous") => "Hello " + name;
+console.log(greeting());
+
