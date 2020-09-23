b3a013c6bac59c1789ae43e6d2725dbd2f743c88
ramankarki <ramankarki40@gmail.com>
2020-09-23 16:16:15 +0545

    fixed some bugs

--- Intermediate algorithm scripting/range.js
+++ Intermediate algorithm scripting/range.js
@@ -1,21 +1,21 @@
-function range(value1, value2) {
-    let start;
-    let end;
-    let numList = [];
-    if (value1 < value2) {
-      start = value1;
-      end = value2;
+function range(start, end = false, step = 1) {
+  let numArray = [];
+  if (typeof start === typeof 1 && end === false) {
+    for (let i = 0; i < start; i++) {
+      numArray.push(i);
     }
-    else {
-      start = value2;
-      end = value1;
+  } else if (typeof start === typeof 1 && typeof end === typeof 1) {
+    if (start < end && step > 0) {
+      for (let i = start; i < end; i += step) {
+        numArray.push(i);
+      }
+    } else if (start > end && step < 0) {
+      for (let i = start; i > end; i += step) {
+        numArray.push(i);
+      }
     }
-    for (let i = start; i < end; i++) {
-      numList.push(i);
-    }
-
-    return numList;
+  }
+  return numArray;
 }
-  
-console.log(range(5,10));
 
+console.log(range(20, 10, -5));
