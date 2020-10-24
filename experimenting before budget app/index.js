11961a759c47b4e38b3aca2fcf538ddc159db15f
ramankarki <ramankarki40@gmail.com>
2020-10-24 13:37:44 +0545

    practicing

---
+++ experimenting before budget app/index.js
@@ -0,0 +1,31 @@
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        let age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedals() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+let john = new Athlete6("john", 1990, "coach", 3, 10);
+
+// john.calculateAge();
+
+
