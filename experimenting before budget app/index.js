0613cedab7503e345ca5899ec8eef0847029fcf1
ramankarki <ramankarki40@gmail.com>
2020-10-29 13:40:58 +0545

    experimenting with object inheritance

--- experimenting before budget app/index.js
+++ experimenting before budget app/index.js
@@ -17,7 +17,7 @@ class Athlete6 extends Person6 {
         this.olympicGames = olympicGames;
         this.medals = medals;
     }
-    
+
     wonMedals() {
         this.medals++;
         console.log(this.medals);
@@ -28,4 +28,51 @@ let john = new Athlete6("john", 1990, "coach", 3, 10);
 
 // john.calculateAge();
 
+// function Shape(name, sides, sideLength) {
+//     this.name = name;
+//     this.sides = sides;
+//     this.sideLength = sideLength;
+// }
+
+// Write your code below here
+// Shape.prototype = {
+//     calcPerimeter: function () {
+//         return this.sides * this.sideLength;
+//     },
+// }
+
+// let square = new Shape("square", 4, 5);
+// console.log(square.calcPerimeter());
+
+// --------------------------------------------------
+
+class Shape {
+    constructor(name, sides, sideLength) {
+        this.name = name;
+        this.sides = sides;
+        this.sideLength = sideLength;
+    };
+
+    calcPerimeter() {
+        return this.sides * this.sideLength;
+    };
+}
+
+let triangle = new Shape("triangle", 3, 3);
+console.log(triangle.calcPerimeter());
+
+class Square extends Shape {
+    constructor(sideLength, name = "square", sides = 4) {
+        super(name, sides, sideLength);
+        this.sideLength = sideLength;
+    }
+
+    calcArea() {
+        return this.sideLength ** 2;
+    }
+}
+
+let square = new Square(5);
+console.log(square.calcArea());
+console.log(square.calcPerimeter());
 
