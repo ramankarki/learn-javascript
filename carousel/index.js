71e832d35fc7f3e711071e41b1e1bd7c1666420d
ramankarki <ramankarki40@gmail.com>
2020-11-07 12:08:03 +0545

    refractored code

--- carousel/index.js
+++ carousel/index.js
@@ -1,88 +1,95 @@
+// DOM element queries
 const carousel = document.querySelector(".carousel");
 const slideContainer = document.querySelector(".slide-container");
 const nextBtn = document.querySelector(".carousel-right-btn");
 const prevBtn = document.querySelector(".carousel-left-btn");
-const dotNavs = document.querySelectorAll(".carousel-indicator");
 const carouselIndicator = Array.from(document.querySelectorAll(".carousel-indicator"));
 const getSlides = () => document.querySelectorAll(".carousel-slide");
 let slides = getSlides();
 
+// carousel setup before starting auto transition
+let currentSlide = 1;
+const interval = 3000;
+let stopSlide;
+let activeSlideId;
 const slideWidth = slides[0].getBoundingClientRect().width;
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
-let currentSlide = 1;
-const interval = 3000;
-let stopSlideInterval;
-
 slideContainer.append(firstClone);
 slideContainer.prepend(lastClone);
 slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+slideTransition();
+
+
+//     ############ functions ###############
 
 function nextSlide() {
     slides = getSlides();
     if (currentSlide >= slides.length - 1) return;
     currentSlide++;
-    slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
-    slideContainer.style.transition = "transform 500ms ease-out";
-    updateIndicator();
+    slideTransition();
 }
 
 function prevSlide() {
     if (currentSlide <= 0) return;
     currentSlide--;
-    slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+    slideTransition();
+}
+
+function slideTransition() {
+    // move slides
     slideContainer.style.transition = "transform 500ms ease-out";
-    updateIndicator();
-}
+    slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
 
-function updateIndicator() {
+    // update indicator
     slides = getSlides();
-    let activeSlide = Number(slides[currentSlide].id.split("-")[1]);
+    activeSlideId = Number(slides[currentSlide].id.split("-")[1]);
     carouselIndicator.forEach(button => {
-        button.classList.remove("active-slide");
-
+        button.classList.remove("active-slide-indicator");
     });
-    carouselIndicator[activeSlide].classList.add("active-slide");
+    carouselIndicator[activeSlideId].classList.add("active-slide-indicator");
 }
 
+
+// ############# event listeners ############
+
+// slides and indicator gets updated when indicator button clicked
 function indicatorBtnClick() {
     carouselIndicator.forEach((btn) => {
         btn.onclick = function () {
             currentSlide = Number(btn.id.split("-")[1]);
-            updateIndicator();
-            slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+            slideTransition();
         }
     })
 }
+indicatorBtnClick();
 
+// slides auto moves every 3 sec
 const startSlide = () => {
-    stopSlideInterval = setInterval(() => {
+    stopSlide = setInterval(() => {
         nextSlide();
     }, interval);
 };
+startSlide();
 
+// checks if current slide is first or last after transition end
 slideContainer.addEventListener("transitionend", () => {
     slides = getSlides();
     if (slides[currentSlide] === firstClone) {
-        slideContainer.style.transition = "none";
         currentSlide = 1;
-        slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
     } else if (slides[currentSlide] === lastClone) {
-        slideContainer.style.transition = "none";
-        currentSlide = 3;
-        slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
+        currentSlide = slides.length - 2;
     }
+    slideContainer.style.transition = "none";
+    slideContainer.style.transform = `translateX(${-slideWidth * currentSlide}px)`;
 });
 
+// slides auto move stops when hover
 carousel.addEventListener("mouseenter", () => {
-    clearInterval(stopSlideInterval);
+    clearInterval(stopSlide);
 });
-
 carousel.addEventListener("mouseleave", startSlide);
+
+// previous and next buttons
 nextBtn.addEventListener("click", nextSlide);
 prevBtn.addEventListener("click", prevSlide);
-startSlide();
-indicatorBtnClick();
-
-
-
