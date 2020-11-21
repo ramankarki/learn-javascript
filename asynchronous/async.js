879e391314534a13c068f130366b09c47afd774d
ramankarki <ramankarki40@gmail.com>
2020-11-21 17:41:08 +0545

    multiple promises at a same time

--- asynchronous/async.js
+++ asynchronous/async.js
@@ -6,7 +6,7 @@
 
 // setInterval(updateTime, 1000);
 
-// -------------------------------------
+// --------------------------------------------------------------------------
 
 // fetch("https://source.unsplash.com/1366x728/?nature,water").then(response => {
 //     if (response.ok) {
@@ -23,47 +23,49 @@
 //     console.log('There has been a problem with your fetch operation: ' + e.message);
 // });
 
-// -------------------------------------
+// --------------------------------------------------------------------------
 
-function fetchAndDecode(url, type) {
-    return fetch(url).then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        } else {
-            if (type === 'blob') {
-                return response.blob();
-            } else if (type === 'text') {
-                return response.text();
-            }
-        }
-    }).catch(e => {
-        console.log(`There has been a problem with your fetch operation for resource "${url}": ` + e.message);
-    });
-}
+// function fetchAndDecode(url, type) {
+//     return fetch(url).then(response => {
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         } else {
+//             if (type === 'blob') {
+//                 return response.blob();
+//             } else if (type === 'text') {
+//                 return response.text();
+//             }
+//         }
+//     }).catch(e => {
+//         console.log(`There has been a problem with your fetch operation for resource "${url}": ` + e.message);
+//     });
+// }
 
-function display(blobUrl, type) {
-    let element = document.createElement(type);
-    if (type === "img") {
-        element.src = blobUrl;
-    }
-    element.textContent = blobUrl;
-    document.body.appendChild(element);
-}
+// function display(blobUrl, type) {
+//     let element = document.createElement(type);
+//     if (type === "img") {
+//         element.src = blobUrl;
+//     }
+//     element.textContent = blobUrl;
+//     document.body.appendChild(element);
+// }
 
-let image = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/coffee.jpg", "blob");
-let image1 = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/tea.jpg", "blob");
-let text = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/description.txt", "text");
+// let image = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/coffee.jpg", "blob");
+// let image1 = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/tea.jpg", "blob");
+// let text = fetchAndDecode("https://raw.githubusercontent.com/mdn/learning-area/master/javascript/asynchronous/promises/description.txt", "text");
 
 
-Promise.all([image, image1, text]).then(values => {
-    console.log(values);
-    // Store each value returned from the promises in separate variables; create object URLs from the blobs
-    let objectURL1 = URL.createObjectURL(values[0]);
-    let objectURL2 = URL.createObjectURL(values[1]);
+// Promise.all([image, image1, text]).then(values => {
+//     console.log(values);
+//     // Store each value returned from the promises in separate variables; create object URLs from the blobs
+//     let objectURL1 = URL.createObjectURL(values[0]);
+//     let objectURL2 = URL.createObjectURL(values[1]);
 
-    // Display the images in <img> elements
-    display(objectURL1, "img");
-    display(objectURL2, "img");
-    display(values[2], "p");
-});
+//     // Display the images in <img> elements
+//     display(objectURL1, "img");
+//     display(objectURL2, "img");
+//     display(values[2], "p");
+// });
+
+// --------------------------------------------------------------------------
 
