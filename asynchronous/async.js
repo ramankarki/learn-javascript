2016f8d54f3f75050e138e9f495db12295c34297
ramankarki <ramankarki40@gmail.com>
2020-11-21 13:25:16 +0545

    asynchronously loading image from unsplash

--- asynchronous/async.js
+++ asynchronous/async.js
@@ -1,8 +1,24 @@
-let updateTime = function() {
-    let time = new Date();
-    document.body.innerHTML = time.toLocaleString();
-    document.body.style.fontFamily = "sans-serif";
-};
+// let updateTime = function() {
+//     let time = new Date();
+//     document.body.innerHTML = time.toLocaleString();
+//     document.body.style.fontFamily = "sans-serif";
+// };
 
-setInterval(updateTime, 1000);
-set
+// setInterval(updateTime, 1000);
+
+fetch("https://source.unsplash.com/1366x728/?nature,water").then(response => {
+    if (response.ok) {
+        return response.blob();
+    } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+}).then (myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElement("img");
+    image.src = objectURL;
+    document.body.append(image);
+}).catch(response => {
+    console.log("Error: " + response.message);
+});
+
+
